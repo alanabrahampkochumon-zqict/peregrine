0982c24f48b78df39fde6df911687adf656af4ee
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-05-02 17:50:57 +0530

    docs: add doxygen groups for the main library

---
+++ memory-manager/include/DoxygenGroups.h
@@ -0,0 +1,28 @@
+#pragma once
+/**
+ * @file DoxygenGroups.h
+ * @author Alan Abraham P Kochumon
+ * @date Created on: May 02, 2026
+ *
+ * @brief Definitions for doxygen groups used across Peregrine Memory library.
+ *
+ * @copyright Copyright (c) 2026 Alan Abraham P Kochumon
+ */
+
+
+// clang-format off
+/**
+ * @defgroup PMM_Lib Peregrine Memory Manager
+ * @brief High performance memory manager suite.
+ * @{
+ */
+    
+    /**
+     * @defgroup PMM_Constants Memory Literals & Constants
+     * @brief Literals for common memory units.
+     * @ingroup PMM_Lib
+     */
+    
+/** @} */ // End of FGM_Math
+
+// clang-format on
--- memory-manager/include/utils/Constants.h
+++ memory-manager/include/utils/Constants.h
@@ -16,69 +16,76 @@
 namespace pmm::constants
 {
 
-	inline constexpr uint64_t BYTE = 1;         ///< Size of a single byte.
-	inline constexpr uint64_t WORD = 2;         ///< Size of a Word (2 bytes).
-	inline constexpr uint64_t DWORD = 4;         ///< Size of a DoubleWord (4 bytes).
-	inline constexpr uint64_t QWORD = 8;         ///< Size of a QuadWord (8 bytes).
+    /**
+     * @addtogroup PMM_Constants
+     * @{
+     */
 
-	inline constexpr uint64_t KB = 1024;         ///< Size of 1 Kilobyte in bytes.
-	inline constexpr uint64_t MB = KB * KB;      ///< Size of 1 Megabyte in bytes.
-	inline constexpr uint64_t GB = KB * MB;      ///< Size of 1 Gigabyte in bytes.
-	inline constexpr uint64_t TB = KB * GB;      ///< Size of 1 Terabyte in bytes.
+    inline constexpr uint64_t BYTE = 1;  ///< Size of a single byte.
+    inline constexpr uint64_t WORD = 2;  ///< Size of a Word (2 bytes).
+    inline constexpr uint64_t DWORD = 4; ///< Size of a DoubleWord (4 bytes).
+    inline constexpr uint64_t QWORD = 8; ///< Size of a QuadWord (8 bytes).
 
-
-	/**
-	 * @brief User-defined literal for Byte assignment.
-	 * @param[in] byte The amount of bytes.
-	 * @return The equivalent size in bytes.
-	 */
-	constexpr uint64_t operator""_B(const unsigned long long byte) noexcept
-	{
-		return byte;
-	}
+    inline constexpr uint64_t KB = 1024;    ///< Size of 1 Kilobyte in bytes.
+    inline constexpr uint64_t MB = KB * KB; ///< Size of 1 Megabyte in bytes.
+    inline constexpr uint64_t GB = KB * MB; ///< Size of 1 Gigabyte in bytes.
+    inline constexpr uint64_t TB = KB * GB; ///< Size of 1 Terabyte in bytes.
 
 
-	/**
-	 * @brief User-defined literal for Kilobyte to byte conversion.
-	 * @param[in] byte The amount of Kilobytes.
-	 * @return The equivalent size in bytes.
-	 */
-	constexpr uint64_t operator""_KB(const unsigned long long byte) noexcept
-	{
-		return byte * KB;
-	}
+    /**
+     * @brief User-defined literal for Byte assignment.
+     * @param[in] byte The amount of bytes.
+     * @return The equivalent size in bytes.
+     */
+    constexpr uint64_t operator""_B(const unsigned long long byte) noexcept
+    {
+        return byte;
+    }
 
 
-	/**
-	 * @brief User-defined literal for Megabyte to byte conversion.
-	 * @param[in] byte The amount of Megabytes.
-	 * @return The equivalent size in bytes.
-	 */
-	constexpr uint64_t operator""_MB(const unsigned long long byte) noexcept
-	{
-		return byte * MB;
-	}
+    /**
+     * @brief User-defined literal for Kilobyte to byte conversion.
+     * @param[in] byte The amount of Kilobytes.
+     * @return The equivalent size in bytes.
+     */
+    constexpr uint64_t operator""_KB(const unsigned long long byte) noexcept
+    {
+        return byte * KB;
+    }
 
 
-	/**
-	 * @brief User-defined literal for Gigabyte to byte conversion.
-	 * @param[in] byte The amount of Gigabytes.
-	 * @return The equivalent size in bytes.
-	 */
-	constexpr uint64_t operator""_GB(const unsigned long long byte) noexcept
-	{
-		return byte * GB;
-	}
+    /**
+     * @brief User-defined literal for Megabyte to byte conversion.
+     * @param[in] byte The amount of Megabytes.
+     * @return The equivalent size in bytes.
+     */
+    constexpr uint64_t operator""_MB(const unsigned long long byte) noexcept
+    {
+        return byte * MB;
+    }
 
 
-	/**
-	 * @brief User-defined literal for Terabyte to byte conversion.
-	 * @param[in] byte The amount of Terabytes.
-	 * @return The equivalent size in bytes.
-	 */
-	constexpr uint64_t operator""_TB(const unsigned long long byte) noexcept
-	{
-		return byte * TB;
-	}
-}
+    /**
+     * @brief User-defined literal for Gigabyte to byte conversion.
+     * @param[in] byte The amount of Gigabytes.
+     * @return The equivalent size in bytes.
+     */
+    constexpr uint64_t operator""_GB(const unsigned long long byte) noexcept
+    {
+        return byte * GB;
+    }
 
+
+    /**
+     * @brief User-defined literal for Terabyte to byte conversion.
+     * @param[in] byte The amount of Terabytes.
+     * @return The equivalent size in bytes.
+     */
+    constexpr uint64_t operator""_TB(const unsigned long long byte) noexcept
+    {
+        return byte * TB;
+    }
+
+    /** @} */
+
+} // namespace pmm::constants
