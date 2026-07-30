c4fac672f1cea793fa9fa97093bf6ce2e755882b
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-07-30 09:40:09 +0530

    test: add tests to verify forward alignment in pool ctor

--- tests/peregrine/allocators/pool/AllocatorTests.cpp
+++ tests/peregrine/allocators/pool/AllocatorTests.cpp
@@ -22,23 +22,36 @@ namespace
      **************************************/
     using namespace pmm::constants;
 
-    /// @test Test fixture for @ref pmm::Pool<pmm::ManagedMemory> tests.
+    /// @brief Test fixture for @ref pmm::Pool<pmm::ManagedMemory> tests.
     struct ManagedPoolAllocator: public testing::Test
     {
         size_t poolSize{ 2_MB }, chunkSize{ 1_KB }, alignment{ 16 };
         pmm::Pool<pmm::ManagedMemory> pool{ poolSize, chunkSize, alignment };
     };
 
-    /// @test Test fixture for @ref pmm::Pool<pmm::UnmanagedMemory> tests.
+    /// @brief Test fixture for @ref pmm::Pool<pmm::UnmanagedMemory> tests.
     struct UnmanagedPoolAllocator: public testing::Test
     {
         size_t bufferSize{ 2_MB }, chunkSize{ 1_KB }, alignment{ 16 };
         uint8_t* buffer = new uint8_t[bufferSize];
         pmm::Pool<pmm::UnmanagedMemory> pool{ buffer, bufferSize, chunkSize, alignment };
     };
-    //
-    // struct PoolAllocatorAlignment{};
 
+
+    /// @brief Parameters for Pool allocator's alignment testing.
+    struct PoolAllocatorAlignmentParams
+    {
+        size_t poolSize, chunkSize, alignment;
+    };
+    /// @brief Parameterized test fixture for @ref pmm::Pool<> base address alignment.
+    class PoolAllocatorAlignment: public testing::TestWithParam<PoolAllocatorAlignmentParams>
+    {};
+    INSTANTIATE_TEST_SUITE_P(
+        PoolAllocatorTests, PoolAllocatorAlignment,
+        testing::Values(PoolAllocatorAlignmentParams{ .poolSize = 4_KB, .chunkSize = 24, .alignment = 8 },
+                        PoolAllocatorAlignmentParams{ .poolSize = 5123, .chunkSize = 32, .alignment = 32 },
+                        PoolAllocatorAlignmentParams{ .poolSize = 9582, .chunkSize = 127, .alignment = 64 },
+                        PoolAllocatorAlignmentParams{ .poolSize = 20_MB, .chunkSize = 2_KB, .alignment = 4_KB }));
 
 
 } // namespace
@@ -62,6 +75,12 @@ namespace
 namespace pmm
 {
 
+    /**************************************
+     *                                    *
+     *           MANAGED STACK            *
+     *                                    *
+     **************************************/
+
     TEST_F(ManagedPoolAllocator, Ctor_InitializesMemberVariables)
     {
         EXPECT_EQ(poolSize, pool._poolSize);
@@ -70,6 +89,33 @@ namespace pmm
         EXPECT_NE(nullptr, pool._buffer);
     }
 
+    // TODO: Add debug death tests
+
+    TEST_P(PoolAllocatorAlignment, Managed_Ctor_AlignsBaseAddress)
+    {
+        const auto [poolSize, chunkSize, alignment] = GetParam();
+        const Pool<> pool{ poolSize, chunkSize, alignment };
+        const auto startAddress = reinterpret_cast<uintptr_t>(pool._buffer) + pool._initialAlignmentPadding;
+
+        EXPECT_EQ(0, startAddress % alignment);
+    }
+
+
+    TEST_P(PoolAllocatorAlignment, Managed_Ctor_AlignsChunksize)
+    {
+        const auto [poolSize, chunkSize, alignment] = GetParam();
+        const Pool<> pool{ poolSize, chunkSize, alignment };
+
+        EXPECT_EQ(0, pool._chunkSize % alignment);
+    }
+
+
+
+    /**************************************
+     *                                    *
+     *          UNMANAGED STACK           *
+     *                                    *
+     **************************************/
 
     TEST_F(UnmanagedPoolAllocator, Ctor_InitializesMemberVariables)
     {
@@ -77,5 +123,26 @@ namespace pmm
         EXPECT_EQ(chunkSize, pool._chunkSize);
         EXPECT_EQ(alignment, pool._chunkAlignment);
         EXPECT_EQ(buffer, pool._buffer);
+    }
+
+
+    TEST_P(PoolAllocatorAlignment, Unmanaged_Ctor_AlignsBaseAddress)
+    {
+        const auto [poolSize, chunkSize, alignment] = GetParam();
+        const auto buffer = new uint8_t[poolSize];
+        const Pool<UnmanagedMemory> pool{ buffer, poolSize, chunkSize, alignment };
+        const auto startAddress = reinterpret_cast<uintptr_t>(pool._buffer) + pool._initialAlignmentPadding;
+
+        EXPECT_EQ(0, startAddress % alignment);
+    }
+
+
+    TEST_P(PoolAllocatorAlignment, Unmanaged_Ctor_AlignsChunksize)
+    {
+        const auto [poolSize, chunkSize, alignment] = GetParam();
+        const auto buffer = new uint8_t[poolSize];
+        const Pool<UnmanagedMemory> pool{ buffer, poolSize, chunkSize, alignment };
+
+        EXPECT_EQ(0, pool._chunkSize % alignment);
     }
 } // namespace pmm
