a72c826c84aea589e3c97fee7177e2ca0c281808
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-08-05 08:38:19 +0530

    feat: add platform specific compilation stubs for memory

--- include/peregrine/memory/LinuxMemory.cpp
+++ include/peregrine/memory/LinuxMemory.cpp
@@ -1,5 +1,5 @@
 /**
- * @file WinMemory.cpp
+ * @file LinuxMemory.cpp
  * @author Alan Abraham P Kochumon
  * @date Created on: August 3, 2026
  *
@@ -8,40 +8,33 @@
  * @copyright Copyright (c) 2026 Alan Abraham P Kochumon
  */
 
-
+// TODO: Update Implementation
 #include "../utils/Preprocessors.h"
 #include "Memory.h"
 
-#ifdef PMM_PLATFORM_WINDOWS
-    #define WIN32_LEAN_AND_MEAN
-    #define NOMINMAX // Prevent windows from hijacking min and max functions
-    #include <Windows.h>
+#ifdef PMM_PLATFORM_LINUX
 
 namespace pmm
 {
-
     void* malloc(const std::size_t byteSize) noexcept // NOLINT(bugprone-exception-escape)
-{
+    {
         PMM_ASSERT_MSG(byteSize > 1, "Cannot allocate less than 1 byte!");
-        return VirtualAlloc(nullptr, byteSize, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-}
+        return new uint8_t[byteSize]; // TODO: Replace
+    }
 
 
     bool mfree(void* start, std::size_t) noexcept // NOLINT(bugprone-exception-escape)
     {
         PMM_ASSERT_MSG(start != nullptr, "Cannot free a nullptr!");
         // Windows requires a dwSize of 0 to free the entire memory block reserved with VirtualAlloc
-        return VirtualFree(start, 0, MEM_RELEASE);
+        // TODO: Implementation
+        return true;
     }
 
     MemoryDetails queryMemoryDetails() noexcept
-    {
-        SYSTEM_INFO info{};
-        GetSystemInfo(&info);
-
-        return MemoryDetails{ .pageSize = info.dwPageSize, .granularity = info.dwAllocationGranularity };
+    { // TODO: Implementation
+        return MemoryDetails{ .pageSize = 4096, .granularity = 4096 };
     }
-
 
 } // namespace pmm
 
